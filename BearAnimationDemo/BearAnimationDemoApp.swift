f629251fc34a83b679ed5e1923e980e2d0e53893
liuhongli <liuhongli2120@gmail.com>
2025-01-03 16:32:43 +0800

    [t0]init

---
+++ BearAnimationDemo/BearAnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearAnimationDemoApp.swift
+//  BearAnimationDemo
+//
+//  Created by admin on 2025/1/3.
+//
+
+import SwiftUI
+
+@main
+struct BearAnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BearAnimationDemoTests/BearAnimationDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  BearAnimationDemoTests.swift
+//  BearAnimationDemoTests
+//
+//  Created by admin on 2025/1/3.
+//
+
+import Testing
+
+struct BearAnimationDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
